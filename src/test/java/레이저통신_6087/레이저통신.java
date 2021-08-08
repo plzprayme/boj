30e1c4c8ded27b52bdb7eb5856c7491fb2d8d393
Hwang Seongchan <dbfpzk142@Gmail.com>
2021-08-08 15:27:15 +0900

    6087 레이저 통신 우선순위 큐

--- src/test/java/레이저통신_6087/레이저통신.java
+++ src/test/java/레이저통신_6087/레이저통신.java
@@ -4,139 +4,115 @@ import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.Objects;
+import java.util.PriorityQueue;
+import java.util.Queue;
+import java.util.Scanner;
+import java.util.StringTokenizer;
 
 import org.junit.jupiter.api.Test;
 
 public class 레이저통신 {
-    private static char[][] map;
-    private static int W;
-    private static int H;
-    // private static List<Integer> answer = new ArrayList<>();
-    private static Set<Integer> answer = new HashSet<>();
+
+    static int W, H;
+    static char map[][];
+    static int[] dx = {0, 0, 1, -1}, dy = {1, -1, 0, 0};
+    static class State implements Comparable<State> {
+        int x, y;
+        int dir;
+        int count;
+
+        public State(int x, int y, int dir, int count) {
+            this.x = x;
+            this.y = y;
+            this.dir = dir;
+            this.count = count;
+        }
+
+        // 우선순위 큐에 카운트가 적은 순으로 정렬되도록
+        @Override
+        public int compareTo(State o) {
+            // 왼쪽이 작으면 -1 같으면 0 크면 1
+            return Integer.compare(count, o.count);
+        }
+    }
 
     @Test
     public static void main(String[] args) throws IOException {
         BufferedReader r = new BufferedReader(new FileReader("C:\\Users\\prayme\\workspace\\boj\\src\\test\\java\\레이저통신_6087\\input.txt"));
-        BufferedWriter w = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(r.readLine());
 
-
-        // 입력
-        String[] WH = r.readLine().split(" ");
-        W = parse(WH[0]);
-        H = parse(WH[1]);
-
-
-        // 지도 입력
-        int x = -2;
-        int y = -2;
+        W = Integer.parseInt(st.nextToken());
+        H = Integer.parseInt(st.nextToken());
         map = new char[H][W];
+
+        boolean isStart = false;
+        State start = null, end = null;
         for (int i = 0; i < H; i++) {
+            map[i] = r.readLine().toCharArray();
+            for (int j = 0; j < W; j++) {
 
-            String row = r.readLine();
-
-            // C의 위치 기억하기
-            if (x == -2) {
-                int index = row.indexOf('C');
-                if (index != -1) {
-                    x = index;
-                    y = i;
+                if (map[i][j] == 'C') {
+                    if (!isStart) {
+                        start = new State(i, j, 0, 0);
+                        isStart = true;
+                    } else {
+                        end = new State(i, j, 0, 0);
+                    }
                 }
+
+            }
+        }
+
+        System.out.println(go(start, end));
+    }
+
+    private static int go(State start, State end) {
+        PriorityQueue<State> pQ = new PriorityQueue<>();
+        // 방향별 visit 확인
+        boolean[][][] visited = new boolean[4][H][W];
+
+        // 시작점으로부터 4방향을 pq에 먼저 넣기
+        for (int d = 0; d < 4; d++) {
+            int xx = start.x + dx[d];
+            int yy = start.y + dy[d];
+
+            // 범위 벗어남
+            if (xx < 0 || yy < 0 || xx >= H || yy >= W) continue;
+
+            // 벽일 때
+            if (map[xx][yy] == '*') continue;
+            pQ.add(new State(xx, yy, d, 0));
+        }
+
+        while (!pQ.isEmpty()) {
+            State now = pQ.poll();
+
+            // 도착 지점 도달
+            if (now.x == end.x && now.y == end.y) return now.count;
+            // 이미 방문한 곳은 패스
+            if (visited[now.dir][now.x][now.y]) continue;
+            visited[now.dir][now.x][now.y] = true;
+
+            // 동서남북 탐색
+            for (int d = 0; d < 4; d++) {
+                int xx = now.x + dx[d];
+                int yy = now.y + dy[d];
+                // 범위 벗어남
+                if (xx < 0 || yy < 0 || xx >= H || yy >= W) continue;
+                if (map[xx][yy] == '*') continue;
+
+                if (now.dir == d) pQ.add(new State(xx, yy, d, now.count));
+                else pQ.add(new State(xx, yy, d, now.count + 1)); // 180도 도는건 왜 카운트가 1 증가?
             }
 
-            map[i] = row.toCharArray();
         }
 
-        // C의 위치 , * 의 위치를 알아내야하나?
-        // 기억해 놓기에는 너무 많다.
-        // 기억해놓더라도 불필요한 연산(순회)가 발생한다.
-
-        // 직접 순회하자
-
-        left(x-1,y,0);
-        right(x+1,y,0);
-        up(x,y+1,0);
-        down(x,y-1,0);
-
-        System.out.println(answer);
+        return 0;
     }
-
-    private static void left(int x, int y, int count) {
-        if (x == -1) return;
-
-        char c = map[y][x];
-        if (c == '*') return;
-
-        if (c == 'C') {
-            answer.add(count);
-            return;
-        }
-
-        left(x-1, y, count);
-        up(x, y+1, count+1);
-        down(x, y-1, count+1);
-    }
-
-    private static void right(int x, int y, int count) {
-        if (x == W) return;
-
-        char c = map[y][x];
-        if (c == '*') return;
-
-        if (c == 'C') {
-            answer.add(count);
-            return;
-        }
-
-        right(x+1, y, count);
-        up(x, y+1, count+1);
-        down(x, y-1, count+1);
-    }
-
-    private static void up(int x, int y, int count) {
-        if (y == H) return;
-
-        char c = map[y][x];
-        if (c == '*') return;
-
-        if (c == 'C') {
-            answer.add(count);
-            return;
-        }
-
-        up(x, y+1, count);
-        left(x-1, y, count+1);
-        right(x+1, y, count+1);
-    }
-
-    private static void down(int x, int y, int count) {
-        if (y == -1) return;
-
-        char c = map[y][x];
-        if (c == '*') return;
-
-        if (c == 'C') {
-            answer.add(count);
-            return;
-        }
-
-        down(x, y -1, count);
-        left(x-1, y, count+1);
-        right(x+1, y, count+1);
-    }
-
-    // private static int horizontal() {
-    //
-    // }
-
-
-
-    private static int parse(String s) {
-        return Integer.parseInt(s);
-    }
-
-
-
 }
