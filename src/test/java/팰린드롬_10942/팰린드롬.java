e4ee262572d28ea7435efedcf25887c4e93a4dcf
Hwang Seongchan <dbfpzk142@Gmail.com>
2021-08-09 20:37:48 +0900

    10942 팰린드롬 N^N

--- src/test/java/팰린드롬_10942/팰린드롬.java
+++ src/test/java/팰린드롬_10942/팰린드롬.java
@@ -9,28 +9,46 @@ import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.Deque;
 import java.util.List;
+import java.util.StringTokenizer;
 
 import org.junit.jupiter.api.Test;
 
 public class 팰린드롬 {
+
+    static int[] numbers;
+
     @Test
     public void main() throws IOException {
-        BufferedReader r = new BufferedReader(new FileReader("C:\\Users\\prayme\\workspace\\boj\\src\\test\\java\\팰린드롬_10942\\input.txt"));
+        BufferedReader r = new BufferedReader(
+            new FileReader("C:\\Users\\prayme\\workspace\\boj\\src\\test\\java\\팰린드롬_10942\\input.txt"));
         BufferedWriter w = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        r.readLine();
+        int N = parse(r.readLine()) + 1;
+        StringTokenizer st = new StringTokenizer(r.readLine());
 
-        List<Integer> numbers = new ArrayList<>(1_000_000);
-        for (String number : r.readLine().split(" ")) {
-            numbers.add(parse(number));
+        numbers = new int[N];
+        for (int i = 1; i < N; i++) {
+            numbers[i] = parse(st.nextToken());
+        }
+
+        // 한번만 순회하면서 구간별 palindrome을 체크한다. 큰 리스트에서 작은 리스트로 좁아지도록
+        // 어떻게..??
+
+        // 구간 별 기억하기
+        boolean[][] isPalindrome = new boolean[N][N];
+        for (int i = 1; i < N; i++) {
+            for (int j = 1; j < N; j++) {
+                isPalindrome[i][j] = validate(i, j);
+            }
         }
 
         int M = parse(r.readLine());
         for (int i = 0; i < M; i++) {
-            String[] indexes = r.readLine().split(" ");
+            st = new StringTokenizer(r.readLine());
+            int start = parse(st.nextToken());
+            int end = parse(st.nextToken());
 
-            List<Integer> splited = numbers.subList(parse(indexes[0]) - 1, parse(indexes[1]));
-            if (isPalindrome(splited)) {
+            if (isPalindrome[start][end]) {
                 w.write("1");
             } else {
                 w.write("0");
@@ -42,19 +60,25 @@ public class 팰린드롬 {
         w.flush();
     }
 
-    private static int parse(String s) {
-        return Integer.parseInt(s);
-    }
+    private static boolean validate(int start, int end) {
+        // s   e e
+        // 0 1 2
+        // 0 1 2 3
+        if (start == end)
+            return true;
 
-    private static boolean isPalindrome(List<Integer> splited) {
-        Deque<Integer> numbers = new ArrayDeque<>(splited);
-
-        while (!numbers.isEmpty()) {
-            if (numbers.size() == 1) return true;
-
-            if (numbers.pollFirst() != numbers.pollLast()) return false;
+        while (start < end) {
+            if (numbers[start] != numbers[end])
+                return false;
+            ++start;
+            --end;
         }
 
         return true;
     }
+
+    private static int parse(String s) {
+        return Integer.parseInt(s);
+    }
+
 }
