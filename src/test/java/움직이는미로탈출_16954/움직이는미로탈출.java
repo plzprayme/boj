05485e13706bb9efe7309c4359a663830dd06d35
Hwang Seongchan <dbfpzk142@Gmail.com>
2021-08-20 11:20:30 +0900

    16954 움직이는미로탈출 시간을 기준으로

--- src/test/java/움직이는미로탈출_16954/움직이는미로탈출.java
+++ src/test/java/움직이는미로탈출_16954/움직이는미로탈출.java
@@ -18,10 +18,9 @@ public class 움직이는미로탈출 {
         int N = 8;
         int[] dy = {-1, -1, -1, 0, 0, 0, 1, 1, 1};
         int[] dx = {-1, 0, 1, -1, 0, 1, -1, 0, 1};
-
-
-        boolean[][] dp = new boolean[N][N];
+        boolean[][][] dp = new boolean[N+1][N][N];
         char[][] map = new char[N][N];
+
         for (int i = 0; i < N; i++) {
             map[i] = r.readLine().toCharArray();
         }
@@ -29,12 +28,13 @@ public class 움직이는미로탈출 {
         Stack<State> q = new Stack<>();
         // 출발점 셋팅
         q.add(new State(0, 7, 0));
+        dp[0][7][0] = true;
 
         // int answer = 0;
         while (!q.isEmpty()) {
             State now = q.pop();
 
-            if (now.time == 8 || now.y == 0) {
+            if (now.time == 7 || now.y == 0) {
                 System.out.println(1);
                 return;
             }
@@ -45,27 +45,17 @@ public class 움직이는미로탈출 {
                 int nt = now.time + 1;
 
                 if (nx < 0 || ny < 0 || nx >= N || ny >= N) continue;
-                if (dp[ny][nx]) continue;
                 if (map[ny][nx] == '#') continue;
-                if (ny > 0 && map[ny - 1][nx] == '#') continue;
+                if (ny - now.time >= 0 && map[ny - now.time][nx] == '#') continue;
+                if (ny - nt >= 0 && map[ny - nt][nx] == '#') continue;
 
-                // dp[ny][nx] = true;
-                q.add(new State(nt, ny,  nx));
-            }
-
-            for (int x = 0; x < N; x++) {
-                map[7][x] = '.';
-            }
-
-            // 벽을 옮기기
-            for (int y = 6; y >= 0; y--) {
-                for (int x = 0; x < N; x++) {
-                    if (map[y][x] == '#') {
-                        map[y][x] = '.';
-                        map[y+1][x] = '#';
-                    }
+                if (!dp[nt][ny][nx]) {
+                    dp[nt][ny][nx] = true;
+                    q.add(new State(nt, ny,  nx));
                 }
+
             }
+
         }
 
         System.out.println(0);
