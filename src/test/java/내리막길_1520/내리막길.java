814dc66d54340bd3f0f3c2ce0f8259a53f06cc5d
Hwang Seongchan <dbfpzk142@Gmail.com>
2021-08-20 18:05:11 +0900

    1520 내리막길 dfs + visited 인데 int형

--- src/test/java/내리막길_1520/내리막길.java
+++ src/test/java/내리막길_1520/내리막길.java
@@ -8,51 +8,53 @@ import java.util.StringTokenizer;
 import org.junit.jupiter.api.Test;
 
 public class 내리막길 {
-    private static int M;
-    private static int N;
-    private static int count = 0;
+    static int Y;
+    static int X;
+
+    static int[][] map;
+    static int[][] visited;
+
     static int[] dx = {0, 0, -1, 1};
     static int[] dy = {-1, 1, 0, 0};
 
-    static int[][] map;
 
     @Test
     public static void main(String[] args) throws IOException {
-        BufferedReader r = new BufferedReader(new FileReader("C:\\Users\\prayme\\workspace\\boj\\src\\test\\java\\내리막길_1520\\input.txt"));
+        BufferedReader r = new BufferedReader(
+            new FileReader("C:\\Users\\prayme\\workspace\\boj\\src\\test\\java\\내리막길_1520\\input.txt"));
 
         StringTokenizer st = new StringTokenizer(r.readLine());
-        M = parse(st.nextToken());
-        N = parse(st.nextToken());
+        Y = parse(st.nextToken());
+        X = parse(st.nextToken());
 
-        map = new int[M][N];
-        for (int y = 0; y < M; y++) {
+        map = new int[Y + 1][X + 1];
+        for (int y = 1; y <= Y; y++) {
             st = new StringTokenizer(r.readLine());
-            for (int x = 0; x < N; x++) {
+            for (int x = 1; x <= X; x++) {
                 map[y][x] = parse(st.nextToken());
             }
         }
 
-        boolean[][] visited = new boolean[M][N];
-        dfs(0, 1, visited.clone()); // 오른쪽
-        dfs(1, 0, visited.clone()); // 아래
+        visited = new int[Y + 1][X + 1];
 
-        System.out.println(count);
+        System.out.println(dfs(1, 1));
     }
 
-    private static void dfs(int y, int x, boolean[][] visited) {
-        visited[y][x] = true;
-        if (x == N - 1 && y == M - 1) { count++; return; }
+    // 참고 https://wootool.tistory.com/83
+    private static int dfs(int y, int x) {
+        if (x == X && y == Y) return 1;
+        if (visited[y][x] > 0) return visited[y][x];
 
         for (int i = 0; i < 4; i++) {
             int nx = x + dx[i];
             int ny = y + dy[i];
 
-            if ( ny < 0 || nx < 0 || ny >= M || nx >= N || visited[ny][nx]) continue;
-            if (map[y][x] > map[ny][nx]) {
-                dfs(ny, nx, visited.clone());
+            if (0 < nx && nx <= X && 0 < ny && ny <= Y && map[y][x] > map[ny][nx]) {
+                visited[y][x] += dfs(ny, nx);
             }
-
         }
+
+        return visited[y][x];
     }
 
     private static int parse(String s) {
