4967f93a79502ef3ad927061ff068cec4969af9a
Hwang Seongchan <dbfpzk142@Gmail.com>
2021-08-21 13:39:09 +0900

    1520 -1 안와봤다 0 와봤다

--- src/test/java/내리막길_1520/내리막길.java
+++ src/test/java/내리막길_1520/내리막길.java
@@ -1,8 +1,11 @@
 package 내리막길_1520;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 import org.junit.jupiter.api.Test;
@@ -22,12 +25,18 @@ public class 내리막길 {
     public static void main(String[] args) throws IOException {
         BufferedReader r = new BufferedReader(
             new FileReader("C:\\Users\\prayme\\workspace\\boj\\src\\test\\java\\내리막길_1520\\input.txt"));
+        BufferedWriter w  =new BufferedWriter(new OutputStreamWriter(System.out));
 
         StringTokenizer st = new StringTokenizer(r.readLine());
         Y = parse(st.nextToken());
         X = parse(st.nextToken());
 
-        map = new int[Y + 1][X + 1];
+        visited = new int[Y+1][X+1];
+        for (int[] row : visited) {
+            Arrays.fill(row, -1);
+        }
+
+        map = new int[Y+1][X+1];
         for (int y = 1; y <= Y; y++) {
             st = new StringTokenizer(r.readLine());
             for (int x = 1; x <= X; x++) {
@@ -35,23 +44,24 @@ public class 내리막길 {
             }
         }
 
-        visited = new int[Y + 1][X + 1];
-
-        System.out.println(dfs(1, 1));
+        w.write(String.valueOf(dfs(1,1)));
+        w.flush();
     }
 
-    // 참고 https://wootool.tistory.com/83
-    private static int dfs(int y, int x) {
+    private static int dfs(int x, int y) {
         if (x == X && y == Y) return 1;
-        if (visited[y][x] > 0) return visited[y][x];
+        else if (visited[y][x] >= 0) return visited[y][x];
 
+        // -1: 안와봤다 0: 와봤다
+        visited[y][x] = 0;
         for (int i = 0; i < 4; i++) {
             int nx = x + dx[i];
             int ny = y + dy[i];
 
             if (0 < nx && nx <= X && 0 < ny && ny <= Y && map[y][x] > map[ny][nx]) {
-                visited[y][x] += dfs(ny, nx);
+                visited[y][x] += dfs(nx, ny);
             }
+
         }
 
         return visited[y][x];
