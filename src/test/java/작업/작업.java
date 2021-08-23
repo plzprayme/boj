9a648426103f8490b83a44af14e1a566e360faa2
Hwang Seongchan <dbfpzk142@Gmail.com>
2021-08-23 15:20:19 +0900

    2056 작업 사전작업 없는 것 처리도 실패

--- src/test/java/작업/작업.java
+++ src/test/java/작업/작업.java
@@ -11,27 +11,34 @@ import org.junit.jupiter.api.Test;
 public class 작업 {
     @Test
     public static void main(String[] args) throws IOException {
-        BufferedReader r = new BufferedReader(new FileReader("C:\\Users\\prayme\\workspace\\boj\\src\\test\\java\\작업\\input.txt"));
+        BufferedReader r = new BufferedReader(
+            new FileReader("C:\\Users\\prayme\\workspace\\boj\\src\\test\\java\\작업\\input.txt"));
 
         int N = parse(r.readLine());
-        int[] dp = new int[N+1];
+        int[] dp = new int[N + 1];
         StringTokenizer st = new StringTokenizer(r.readLine());
         dp[1] = parse(st.nextToken());
 
+        int max = 0;
         for (int i = 2; i < N + 1; i++) {
             st = new StringTokenizer(r.readLine());
             int time = parse(st.nextToken());
             int pre = parse(st.nextToken());
 
-            int longest = 0;
-            for (int j = 0; j < pre; j++) {
-                int index = parse(st.nextToken());
-                longest = Math.max(longest, dp[index]);
+            if (pre == 0) {
+                max = Math.max(max, time);
+            } else {
+                int longest = 0;
+                for (int j = 0; j < pre; j++) {
+                    int index = parse(st.nextToken());
+                    longest = Math.max(longest, dp[index]);
+                }
+                dp[i] = longest + time;
             }
-            dp[i] = longest + time;
+
         }
 
-        System.out.println(dp[N]);
+        System.out.println(Math.max(dp[N], max));
     }
 
     private static int parse(String s) {
