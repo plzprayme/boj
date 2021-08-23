40a9a4971ff3cea7ff60942aa952ce11bca77086
Hwang Seongchan <dbfpzk142@Gmail.com>
2021-08-23 14:43:25 +0900

    2056 작업 틀렸대

---
+++ src/test/java/작업/작업.java
@@ -0,0 +1,40 @@
+package 작업;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+import org.junit.jupiter.api.Test;
+
+public class 작업 {
+    @Test
+    public static void main(String[] args) throws IOException {
+        BufferedReader r = new BufferedReader(new FileReader("C:\\Users\\prayme\\workspace\\boj\\src\\test\\java\\작업\\input.txt"));
+
+        int N = parse(r.readLine());
+        int[] dp = new int[N+1];
+        StringTokenizer st = new StringTokenizer(r.readLine());
+        dp[1] = parse(st.nextToken());
+
+        for (int i = 2; i < N + 1; i++) {
+            st = new StringTokenizer(r.readLine());
+            int time = parse(st.nextToken());
+            int pre = parse(st.nextToken());
+
+            int longest = 0;
+            for (int j = 0; j < pre; j++) {
+                int index = parse(st.nextToken());
+                longest = Math.max(longest, dp[index]);
+            }
+            dp[i] = longest + time;
+        }
+
+        System.out.println(dp[N]);
+    }
+
+    private static int parse(String s) {
+        return Integer.parseInt(s);
+    }
+}
