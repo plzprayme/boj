60699c6ed3d84a06d8fbd7d019ade5b369067be1
Hwang Seongchan <dbfpzk142@Gmail.com>
2021-08-23 15:39:58 +0900

    2056 작업 전부 다 사전 작업 없을때도 실패

--- src/test/java/작업/작업.java
+++ src/test/java/작업/작업.java
@@ -9,24 +9,24 @@ import java.util.StringTokenizer;
 import org.junit.jupiter.api.Test;
 
 public class 작업 {
+
+    static BufferedReader r;
+
     @Test
     public static void main(String[] args) throws IOException {
-        BufferedReader r = new BufferedReader(
+        r = new BufferedReader(
             new FileReader("C:\\Users\\prayme\\workspace\\boj\\src\\test\\java\\작업\\input.txt"));
 
         int N = parse(r.readLine());
         int[] dp = new int[N + 1];
-        StringTokenizer st = new StringTokenizer(r.readLine());
-        dp[1] = parse(st.nextToken());
 
-        int max = 0;
-        for (int i = 2; i < N + 1; i++) {
-            st = new StringTokenizer(r.readLine());
+        for (int i = 1; i < N + 1; i++) {
+            StringTokenizer st = new StringTokenizer(r.readLine());
             int time = parse(st.nextToken());
             int pre = parse(st.nextToken());
 
             if (pre == 0) {
-                max = Math.max(max, time);
+                dp[i] = time;
             } else {
                 int longest = 0;
                 for (int j = 0; j < pre; j++) {
@@ -38,7 +38,37 @@ public class 작업 {
 
         }
 
-        System.out.println(Math.max(dp[N], max));
+        System.out.println(dp[N]);
+    }
+
+    @Test
+    public void 사전작업_없음() throws IOException {
+        r = new BufferedReader(new FileReader("C:\\Users\\prayme\\workspace\\boj\\src\\test\\java\\작업\\전부다사전작업없음.txt"));
+
+        int N = parse(r.readLine());
+        int[] dp = new int[N + 1];
+
+        int max = 0;
+        for (int i = 1; i < N + 1; i++) {
+            StringTokenizer st = new StringTokenizer(r.readLine());
+            int time = parse(st.nextToken());
+            int pre = parse(st.nextToken());
+
+            if (pre == 0) {
+                dp[i] = time;
+                max = Math.max(time, max);
+            } else {
+                int longest = 0;
+                for (int j = 0; j < pre; j++) {
+                    int index = parse(st.nextToken());
+                    longest = Math.max(longest, dp[index]);
+                }
+                dp[i] = longest + time;
+            }
+
+        }
+
+        System.out.println(Math.max(max, dp[N]));
     }
 
     private static int parse(String s) {
