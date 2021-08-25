95ac2bd918429d997d375afecd19abed65224eab
Hwang Seongchan <dbfpzk142@Gmail.com>
2021-08-25 17:28:56 +0900

    9177 단어섞기 조합 계산 후 캐싱 당연히 시간초과

--- src/test/java/단어섞기_9177/단어섞기.java
+++ src/test/java/단어섞기_9177/단어섞기.java
@@ -6,7 +6,11 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
 import java.math.BigInteger;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
 import java.util.PriorityQueue;
+import java.util.Set;
 import java.util.Stack;
 import java.util.StringTokenizer;
 
@@ -14,12 +18,15 @@ import org.junit.jupiter.api.Test;
 
 public class 단어섞기 {
 
+    static Map<String, Set<String>> cache;
+
     @Test
     public void main() throws IOException {
         BufferedReader r = new BufferedReader(
             new FileReader("C:\\Users\\prayme\\workspace\\boj\\src\\test\\java\\단어섞기_9177\\input.txt"));
         BufferedWriter w = new BufferedWriter(new OutputStreamWriter(System.out));
 
+        cache = new HashMap<>();
         int N = Integer.parseInt(r.readLine());
         for (int i = 1; i <= N; i++) {
             StringTokenizer st = new StringTokenizer(r.readLine());
@@ -27,49 +34,63 @@ public class 단어섞기 {
             String s2 = st.nextToken();
             String s3 = st.nextToken();
 
-            w.write(String.format("Data set %d: %s", i, validate(s1, s2, s3)));
+            String key = s1.concat(s2);
+            if (!cache.containsKey(key)) {
+                cache.put(key, new HashSet<>());
+                go(s1, s2);
+            }
+            w.write(String.format("Data set %d: %s", i, validate(s3, key)));
             w.newLine();
         }
         w.flush();
     }
 
-    private static String validate(String s1, String s2, String s3) {
-        PriorityQueue<State> pq = new PriorityQueue<>();
-        pq.add(new State(0, 0, 0));
-
-        while (!pq.isEmpty()) {
-            State now = pq.poll();
-            int i1 = now.i1;
-            int i2 = now.i2;
-            int i3 = now.i3;
-            if (i3 == s3.length()) return "yes";
-
-            char c = s3.charAt(i3);
-            if (i1 < s1.length() && i2 < s2.length() &&  c == s1.charAt(i1) && c == s2.charAt(i2)) {
-                pq.add(new State(i1 + 1, i2, i3 + 1));
-                pq.add(new State(i1, i2 + 1, i3 + 1));
-            } else if (i1 < s1.length() && c == s1.charAt(i1)) {
-                pq.add(new State(i1 + 1, i2, i3 + 1));
-            } else if (i2 < s2.length() && c == s2.charAt(i2)) {
-                pq.add(new State(i1, i2 + 1, i3 + 1));
-            }
-        }
-
-        return "no";
+    private static String validate(String s3, String key) {
+        return cache.get(key).contains(s3) ? "yes" : "no";
     }
 
-    private static class State implements Comparable<State> {
-        int i1, i2, i3;
+    private static void go(String s1, String s2) {
+        Stack<State> s = new Stack<>();
+        s.push(new State(0, 0, new StringBuilder()));
+        String key = s1.concat(s2);
 
-        public State(int i1, int i2, int i3) {
+        int length = s1.length() + s2.length();
+
+        while (!s.empty()) {
+            State now = s.pop();
+            int i1 = now.i1;
+            int i2 = now.i2;
+            StringBuilder sb = now.s;
+            if (sb.length() == length) {
+                Set<String> temp = cache.get(key);
+                temp.add(sb.toString());
+                cache.put(key, temp);
+            }
+
+            if (i1 < s1.length() && i2 < s2.length() && s1.charAt(i1) == s2.charAt(i2)) {
+                StringBuilder temp = sb.append(s1.charAt(i1));
+                s.add(new State(i1 + 1, i2, temp));
+                s.add(new State(i1, i2 + 1, temp));
+            } else {
+                if (i1 < s1.length()) {
+                    s.add(new State(i1 + 1, i2, new StringBuilder(sb).append(s1.charAt(i1))));
+                }
+
+                if (i2 < s2.length()) {
+                    s.add(new State(i1, i2 + 1, new StringBuilder(sb).append(s2.charAt(i2))));
+                }
+            }
+        }
+    }
+
+    private static class State {
+        int i1, i2;
+        StringBuilder s;
+
+        public State(int i1, int i2, StringBuilder sb) {
             this.i1 = i1;
             this.i2 = i2;
-            this.i3 = i3;
-        }
-
-        @Override
-        public int compareTo(State o) {
-            return Integer.compare(o.i3, i3);
+            s = sb;
         }
     }
 }
