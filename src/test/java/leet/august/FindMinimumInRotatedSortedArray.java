e430013509ce3abf79646dd8fc9626b8d63cbc16
Hwang Seongchan <dbfpzk142@Gmail.com>
2021-08-31 19:45:31 +0900

    굿바이 8월 릿코드 챌린지

---
+++ src/test/java/leet/august/FindMinimumInRotatedSortedArray.java
@@ -0,0 +1,25 @@
+package leet.august;
+
+import java.util.Arrays;
+
+import org.junit.jupiter.api.Test;
+
+public class FindMinimumInRotatedSortedArray {
+    public int findMin(int[] nums) {
+        int left = 0;
+        int right = nums.length - 1;
+
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] > nums[right]) {
+                left = mid + 1;
+            } else if (nums[mid] < nums[right]) {
+                right = mid;
+            } else {
+                return nums[right];
+            }
+        }
+        return nums[left];
+    }
+}
