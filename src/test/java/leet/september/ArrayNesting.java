bd247834c923ac52e80c1a6b1e26723f5b761f12
Hwang Seongchan <dbfpzk142@Gmail.com>
2021-09-01 20:46:59 +0900

    9/1 dp + dfs

--- src/test/java/leet/september/ArrayNesting.java
+++ src/test/java/leet/september/ArrayNesting.java
@@ -7,24 +7,24 @@ import org.junit.jupiter.api.Test;
 public class ArrayNesting {
     @Test
     public void main() {
-        arrayNesting(new int[]{5, 4, 0, 3, 1, 6, 2});
+        System.out.println(arrayNesting(new int[]{5, 4, 0, 3, 1, 6, 2}));
+        System.out.println(arrayNesting(new int[]{0, 1, 2}));
+        System.out.println(arrayNesting(new int[]{0, 2, 1}));
     }
 
-    boolean[] visited;
     int[] dp;
     public int arrayNesting(int[] nums) {
         dp = new int[nums.length];
         for (int i = 0; i < nums.length; i++) {
-            visited = new boolean[nums.length];
-            dp[i] = dfs(nums.clone(), i, 0);
+            dfs(nums.clone(), i, 0);
         }
         return Arrays.stream(dp).max().getAsInt();
     }
 
-    private int dfs(int[] nums, int index, int count) {
+    private void dfs(int[] nums, int index, int count) {
         int num = nums[index];
-        if (visited[num]) return count;
-        visited[num] = true;
-        return dfs(nums, num, ++count);
+        if (dp[num] > 0)  return;
+        dp[num] = count;
+        dfs(nums, num, ++count);
     }
 }
