8539ac10707e704d55f654b2b32fe8e919d0f6d1
Hwang Seongchan <dbfpzk142@Gmail.com>
2021-09-25 15:37:04 +0900

    N퀸 1차원 배열

--- src/test/java/N퀸_9663/N퀸.java
+++ src/test/java/N퀸_9663/N퀸.java
@@ -3,12 +3,13 @@ package N퀸_9663;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.Arrays;
 
 import org.junit.jupiter.api.Test;
 
 public class N퀸 {
     static int N;
-    static boolean[][] map;
+    static int[] map;
     static int count;
 
     @Test
@@ -16,7 +17,8 @@ public class N퀸 {
         BufferedReader r = new BufferedReader(new FileReader("C:\\Users\\prayme\\workspace\\boj\\src\\test\\java\\N퀸_9663\\input.txt"));
 
         N = Integer.parseInt(r.readLine());
-        map = new boolean[N][N];
+        map = new int[N];
+        Arrays.fill(map, -1);
 
         rec(0);
 
@@ -37,24 +39,21 @@ public class N퀸 {
             for (int x = 0; x < N; x++) {
                 if (isAttackable(y, x)) continue;
 
-                map[y][x] = true;
+                map[y] = x;
                 rec(y + 1);
-                map[y][x] = false;
+                map[y] = -1;
             }
 
         }
     }
 
     private static boolean isAttackable(int y, int x) {
-        int _y = y - 1;
-        int _lx = x - 1;
-        int _rx = x + 1;
-        while (0 <= _y) {
-            if (map[_y][x]) return true;
-            if (_lx >= 0 && map[_y][_lx]) return true;
-            if (_rx < N && map[_y][_rx]) return true;
-
-            _y--; _lx--; _rx++;
+        int l = y - x;
+        int r = y + x;
+        for (int _y = y; _y >= 0; _y--) {
+            if (map[_y] == x) return true;
+            if (_y - map[_y] == l) return true;
+            if (_y + map[_y] == r) return true;
         }
 
         return false;
