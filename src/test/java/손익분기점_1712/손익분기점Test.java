ba7dd37cc5553666080f35d10d50e4410cc71985
Hwang Seongchan <dbfpzk142@Gmail.com>
2021-10-03 18:08:18 +0900

    손익분기점 1712

--- src/test/java/손익분기점_1712/손익분기점Test.java
+++ src/test/java/손익분기점_1712/손익분기점Test.java
@@ -2,30 +2,27 @@ package 손익분기점_1712;
 
 import java.io.BufferedReader;
 import java.io.Closeable;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.nio.Buffer;
+import java.util.StringTokenizer;
 
 import org.junit.jupiter.api.Test;
 
 public class 손익분기점Test {
     @Test
-    public void 예제() {
+    public void 예제() throws IOException {
+        BufferedReader r = new BufferedReader(new FileReader("C:\\Users\\prayme\\workspace\\boj\\src\\test\\java\\손익분기점_1712\\input.txt"));
 
-        String s = "1000 70 170";
-        // String s = "2100000000 9 10";
-        String[] ss = s.split(" ");
-        int A = parse(ss[0]);
-        int B = parse(ss[1]);
-        int C = parse(ss[2]);
+        StringTokenizer st = new StringTokenizer(r.readLine());
+        int A = parse(st.nextToken());
+        int B = parse(st.nextToken());
+        int C = parse(st.nextToken());
+
         int D = C - B;
-        if (D <= 0) {
-            System.out.println("-1");
-            return;
-        }
-
-        System.out.println((A / D) + 1);
-
+        if (D <= 0) System.out.println(-1);
+        else System.out.println(A / D + 1);
     }
 
     private int parse(String s) {
