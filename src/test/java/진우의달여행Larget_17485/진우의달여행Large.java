41d314a6ca0d0e885beb39434ff11bd72d20e9b0
Hwang Seongchan <dbfpzk142@Gmail.com>
2021-10-28 01:03:35 +0900

    진우의달여행 Large 17485 왜틀렸는지 진짜 모르겠음

--- src/test/java/진우의달여행Larget_17485/진우의달여행Large.java
+++ src/test/java/진우의달여행Larget_17485/진우의달여행Large.java
@@ -12,50 +12,56 @@ class 진우의달여행Large {
     static int[][][] dp;
 
     private static void solution() {
-        for (int d = 0; d < 3; d++) {
-            for (int x = 1; x <= M; x++) dp[d][1][x] = fuels[1][x];
-        }
-
         for (int y = 1; y <= N; y++) {
-            dp[2][y][M] = 101;
-            dp[0][y][1] = 101;
-        }
-
-        for (int y = 2; y <= N; y++) {
             for (int x = 1; x <= M; x++) {
-                if (x == 1) { // 가장 왼쪽일 때
-                    dp[1][y][x] = dp[0][y - 1][x + 1] + fuels[y][x]; // 직진할 때는 왼쪽으로 출발한 녀석만 추가 가능
-                    dp[2][y][x] = Math.min(dp[0][y - 1][x + 1], dp[1][y - 1][x]) + fuels[y][x]; // 좌회전 한 녀석과 직진 한 녀석 중 작은 녀석을 추가 가능
-                } else if (x == M) { // 가장 오른쪽 일 때
-                    dp[1][y][x] = dp[2][y - 1][x -1] + fuels[y][x]; // 직진할 때는 우회전 한 녀석만 추가 가능
-                    dp[0][y][x] = Math.min(dp[2][y-1][x-1], dp[1][y-1][x]) + fuels[y][x]; // 좌회전 할 때는 우회전 한 셔거이나 직진한 녀석만 추가 가능
-                } else {
-                    dp[0][y][x] = Math.min(dp[1][y-1][x], dp[2][y-1][x-1]) + fuels[y][x];
-                    dp[1][y][x] = Math.min(dp[0][y-1][x+1], dp[2][y-1][x-1]) + fuels[y][x];
-                    dp[2][y][x] = Math.min(dp[0][y-1][x+1], dp[1][y-1][x]) + fuels[y][x];
+                for (int d = 0; d < 3; d++) {
+                    dp[d][y][x] = getBestWay(y - 1, x, d) + fuels[y][x];
                 }
             }
         }
 
         int min = Integer.MAX_VALUE;
-        for (int x = 1, m = M + 1; x < m; x++) {
+        for (int x = 1; x <= M; x++) {
             for (int d = 0; d < 3; d++) min = Math.min(min, dp[d][N][x]);
         }
         System.out.println(min);
+    }
+
+    // 0 : ↙
+    // 1 : ↓
+    // 2 : ↘
+    private static int getBestWay(int y, int x, int d) {
+        if (x == 1 && d == 0) return 101;
+        if (x == M && d == 2) return 101;
+        int[] dx = {1, 0, -1};
+
+        int d1, d2;
+        if (d == 0) { d1 = 1; d2 = 2; }
+        else if (d == 1) { d1 = 0; d2 = 2; }
+        else { d1 = 0; d2 = 1; }
+
+
+        return Math.min(dp[d1][y][x + dx[d1]], dp[d2][y][x + dx[d2]]);
     }
 
     private static void input() throws IOException {
         BufferedReader r = new BufferedReader(
             new FileReader("C:\\Users\\prayme\\workspace\\boj\\src\\test\\java\\진우의달여행Small_17484\\input.txt"));
         StringTokenizer st = new StringTokenizer(r.readLine());
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
-        dp = new int[3][N + 1][M + 1];
-        fuels = new int[N + 1][M + 1];
+        N = Integer.parseInt(st.nextToken()); M = Integer.parseInt(st.nextToken());
+        dp = new int[3][N + 1][M + 2]; fuels = new int[N + 1][M + 1];
 
-        for (int y = 1, n = N + 1; y < n; y++) {
+        for (int y = 1; y <= N; y++) {
+            dp[2][y][0] = 101;
+            dp[0][y][1] = 101;
+            dp[2][y][M] = 101;
+            dp[0][y][M + 1] = 101;
+        }
+
+
+        for (int y = 1; y <= N; y++) {
             st = new StringTokenizer(r.readLine());
-            for (int x = 1, m = M + 1; x < m; x++) {
+            for (int x = 1; x <= M; x++) {
                 fuels[y][x] = Integer.parseInt(st.nextToken());
             }
         }
