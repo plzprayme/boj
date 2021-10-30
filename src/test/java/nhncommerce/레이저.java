ba9f57c9421da26da4be3c05580b754b0f5759fa
Hwang Seongchan <dbfpzk142@Gmail.com>
2021-10-30 22:40:55 +0900

    nhn 3번 리팩

--- src/test/java/nhncommerce/레이저.java
+++ src/test/java/nhncommerce/레이저.java
@@ -12,16 +12,19 @@ class 레이저 {
     static int[] idx;
     static int[] selected;
     static int answer = Integer.MIN_VALUE;
+
     @Test
     private static void input() throws IOException {
-        BufferedReader r = new BufferedReader(new FileReader("C:\\Users\\prayme\\workspace\\boj\\src\\test\\java\\nhncommerce\\input2.txt"));
+        BufferedReader r = new BufferedReader(
+            new FileReader("C:\\Users\\prayme\\workspace\\boj\\src\\test\\java\\nhncommerce\\input2.txt"));
         StringTokenizer st = new StringTokenizer(r.readLine());
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
         K = Integer.parseInt(st.nextToken());
 
         nums = new int[N][M];
-        selected = new int[N]; idx = new int[N];
+        selected = new int[N];
+        idx = new int[N];
         for (int i = 0; i < N; i++) {
             st = new StringTokenizer(r.readLine());
             for (int j = 0; j < M; j++) {
@@ -29,26 +32,23 @@ class 레이저 {
             }
         }
 
-        combination(0);
+        combination(0, 0);
         System.out.println(answer);
     }
 
-    static private void combination(int n) {
+    static private void combination(int n, int k) {
         if (n == N) {
-            int move = 0;
+            if (k != K) return;
             int sum = 0;
             for (int i = 0; i < N; i++) {
-                move += M - idx[i];
-                sum += nums[i][idx[i]];
+                sum += selected[i];
             }
-            if (move == K) {
-                answer = Math.max(answer, sum);
-            }
+            answer = Math.max(answer, sum);
         } else {
             for (int i = 0; i < M; i++) {
+                if (k + M - i > K) continue;
                 selected[n] = nums[n][i];
-                idx[n] = i;
-                combination(n + 1);
+                combination(n + 1, k + M - i);
             }
         }
     }
