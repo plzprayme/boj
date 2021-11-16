04f6a0ecd5db394966420ff20a635cd3f1ebedfc
Hwang Seongchan <dbfpzk142@Gmail.com>
2021-11-16 20:47:09 +0900

    클로버3 수정

--- src/test/java/네이버클로버/_3.java
+++ src/test/java/네이버클로버/_3.java
@@ -1,5 +1,6 @@
 package 네이버클로버;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -7,9 +8,42 @@ import org.junit.jupiter.api.Test;
 
 public class _3 {
 
+    public int solution2(String s) {
+        int[][] pair = new int[s.length()][2];
+
+        int[] idx = new int[26];
+        Arrays.fill(idx, -1);
+        for (int cur = 0; cur < s.length(); cur++) {
+            int i = s.charAt(cur) - 'A';
+            if (idx[i] == -1) {
+                pair[cur][0] = cur + 1;
+            } else {
+                pair[cur][0] = cur - idx[i];
+            }
+            idx[i] = cur;
+        }
+
+        Arrays.fill(idx, -1);
+        int cur = 0;
+        for (int j = s.length() - 1; j >= 0; j--) {
+            int i = s.charAt(j) - 'A';
+            if (idx[i] == -1) {
+                pair[j][1] = cur + 1;
+            } else {
+                pair[j][1] = cur - idx[i];
+            }
+            idx[i] = cur;
+            cur ++;
+        }
+
+        int answer = 0;
+        for (int[] lr : pair) {
+            answer += lr[0] * lr[1];
+        }
+        return answer;
+    }
 
     public int solution(String s) {
-
         int answer = s.length();
         for (int window = 1; window < s.length(); window++) {
 
@@ -27,31 +61,29 @@ public class _3 {
                     if (j == 1) a++;
                 }
                 answer = (a + answer) % 1_000_000_007;
-
             }
 
         }
-
-
         return answer;
     }
 
     @Test
     public void main() {
         System.out.println(
-            solution("ACAX")
+            solution2("ACAX")
         );
 
         System.out.println(
-            solution("CODILITY")
-        );
-        System.out.println(
-            solution("A")
-        );
-        System.out.println(
-            solution("AA")
+            solution2("CODILITY")
         );
 
+        System.out.println(
+            solution2("A")
+        );
 
+        System.out.println(
+            solution2("AA")
+        );
     }
+
 }
