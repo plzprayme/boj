4b4b11965b65954ee7f694aefd7dd1abd16b5f61
Hwang Seongchan <dbfpzk142@Gmail.com>
2021-11-19 15:34:19 +0900

    수찾기 버그 해결

--- src/test/java/수찾기_1920/수찾기.java
+++ src/test/java/수찾기_1920/수찾기.java
@@ -1,42 +1,81 @@
 package 수찾기_1920;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.Arrays;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 import org.junit.jupiter.api.Test;
 
-public class 수찾기 {
-    @Test
-    public static void main(String[] args) throws IOException {
-        BufferedReader r = new BufferedReader(new FileReader("C:\\Users\\prayme\\workspace\\boj\\src\\test\\java\\수찾기_1920\\input.txt"));
+class 수찾기 {
+
+    static int N, M;
+    static int[] A, B;
+
+    private static void solution() {
         StringBuilder sb = new StringBuilder();
-
-        int N = Integer.parseInt(r.readLine());
-        int[] A = new int[N + 1];
-        StringTokenizer st = new StringTokenizer(r.readLine());
-        for (int i = 1; i <= N; i++) A[i] = Integer.parseInt(st.nextToken());
-        Arrays.sort(A);
-
-        int M = Integer.parseInt(r.readLine());
-        int[] B = new int[M + 1];
-        st = new StringTokenizer(r.readLine());
-        for (int i = 1; i <= N; i++) B[i] = Integer.parseInt(st.nextToken());
-
-
-        for (int i = 1; i <= M; i++) sb.append(bSearch(A, 1, N, B[i])).append('\n');
+        for (int i = 1; i <= M; i++) {
+            sb.append(binarySearch(1, N, B[i])).append('\n');
+        }
         System.out.println(sb);
     }
 
-    static int bSearch(int[] B, int L, int R, int X) {
-        while (L <= R) {
-            int M = L + (R - L) / 2;
-            if (X > B[M]) L = M + 1;
-            else if (X < B[M]) R = M - 1;
-            else return 1;
+    private static int binarySearch(int l, int r, int v) {
+
+        while (l <= r) {
+            int mid = l + (r - l) / 2;
+            if (A[mid] == v) return 1;
+            else if (A[mid] > v) r = mid - 1;
+            else l = mid + 1;
         }
+
         return 0;
     }
+
+    private static void input() throws IOException {
+        InputReader r = new InputReader("C:\\Users\\prayme\\workspace\\boj\\src\\test\\java\\수찾기_1920\\input.txt");
+        N = r.nextInt(); A = new int[N + 1];
+        for (int i = 1; i <= N; i++) {
+            A[i] = r.nextInt();
+        }
+        Arrays.sort(A, 1, N + 1);
+
+        M = r.nextInt(); B = new int[M + 1];
+        for (int i = 1; i <= M; i++) {
+            B[i] = r.nextInt();
+        }
+    }
+
+    @Test
+    public static void main(String[] args) throws IOException {
+        input();
+        solution();
+    }
+
+    private static class InputReader {
+        StringTokenizer st;
+        BufferedReader r;
+
+        public InputReader(String filePath) throws FileNotFoundException {
+            this(new FileReader(filePath));
+        }
+
+        public InputReader() {
+            this(new InputStreamReader(System.in));
+        }
+
+        private InputReader(InputStreamReader reader) {
+            r = new BufferedReader(reader);
+            st = new StringTokenizer("");
+        }
+
+        public int nextInt() throws IOException {
+            if (!st.hasMoreTokens()) st = new StringTokenizer(r.readLine());
+            return Integer.parseInt(st.nextToken());
+        }
+
+        public char[] nextCharArr() throws IOException {
+            return r.readLine().toCharArray();
+        }
+    }
+
 }
+
