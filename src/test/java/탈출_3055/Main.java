df985cfb9a517a84d48ac1f3a03b996549d80b51
Hwang Seongchan <dbfpzk142@Gmail.com>
2021-11-28 21:05:59 +0900

    탈출 3055

--- src/test/java/탈출_3055/Main.java
+++ src/test/java/탈출_3055/Main.java
@@ -9,72 +9,95 @@ class Main {
 
     static int R, C;
     static String[] map;
-    static Queue<Coordinate> queue = new LinkedList<>();
-    static Coordinate hog;
 
-    static int[][] waterDist;
+    static int[][] waterDist, hogDist;
+    static boolean[][] visited;
 
     static int[] dx = {0, 1, 0, -1};
     static int[] dy = {-1, 0, 1, 0};
 
-    static final char WATER = '*';
-    static final char CAVE = 'D';
-    static final char ROCK = 'X';
-    static final char HOG = 'S';
-
     private static void solution() {
         waterBfs();
-        int answer = hogBfs();
-        if (answer == -1) {
-            System.out.println("KAKTUS");
-            return;
-        }
-        System.out.println(answer);
-    }
-
-    private static int hogBfs() {
-        queue.add(hog);
-
-        while (!queue.isEmpty()) {
-            Coordinate cur = queue.poll();
-
-            for (int i = 0; i < 4; i++) {
-                int nx = cur.x + dx[i];
-                int ny = cur.y + dy[i];
-                int nc = cur.c + 1;
-
-                if (isOut(nx, ny) || isRock(nx, ny)) continue;
-
-                if (isCave(nx,ny)) return nc;
-
-                if (waterDist[ny][nx] > nc) {
-                    queue.add(new Coordinate(nx, ny, nc));
-                }
-            }
-        }
-
-        return -1;
+        hogBfs();
+        printAnswer();
     }
 
     private static void waterBfs() {
-        while (!queue.isEmpty()) {
-            Coordinate cur = queue.poll();
-            waterDist[cur.y][cur.x] = Math.min(waterDist[cur.y][cur.x], cur.c);
-
-            for (int i = 0; i < 4; i++) {
-                int nx = cur.x + dx[i];
-                int ny = cur.y + dy[i];
-                int nc = cur.c + 1;
-
-                if (isOut(nx, ny) || isRock(nx, ny) || isCave(nx, ny)) continue;
-
-                if (waterDist[ny][nx] > nc) {
-                    queue.add(new Coordinate(nx, ny, nc));
+        Queue<Coordinate> queue = new LinkedList<>();
+        for (int y = 0; y < R; y++) {
+            for (int x = 0; x < C; x++) {
+                waterDist[y][x] = -1;
+                if (map[y].charAt(x) == '*') {
+                    queue.add(new Coordinate(x, y));
+                    waterDist[y][x] = 0;
+                    visited[y][x] = true;
                 }
             }
         }
 
+        while (!queue.isEmpty()) {
+            Coordinate cur = queue.poll();
+
+            for (int i = 0; i < 4; i++) {
+                int nx = cur.x + dx[i];
+                int ny = cur.y + dy[i];
+
+                if (isOut(nx, ny)) continue;
+                if (visited[ny][nx]) continue;
+                if (map[ny].charAt(nx) != '.') continue;
+
+                visited[ny][nx] = true;
+                waterDist[ny][nx] = waterDist[cur.y][cur.x] + 1;
+                queue.add(new Coordinate(nx, ny));
+            }
+        }
+
     }
+
+    private static void hogBfs() {
+        Queue<Coordinate> queue = new LinkedList<>();
+        for (int y = 0; y < R; y++) {
+            for (int x = 0; x < C; x++) {
+                hogDist[y][x] = -1;
+                visited[y][x] = false;
+                if (map[y].charAt(x) == 'S') {
+                    queue.add(new Coordinate(x, y));
+                    hogDist[y][x] = 0;
+                    visited[y][x] = true;
+                }
+            }
+        }
+
+        while (!queue.isEmpty()) {
+            Coordinate cur = queue.poll();
+
+            for (int i = 0; i < 4; i++) {
+                int nx = cur.x + dx[i];
+                int ny = cur.y + dy[i];
+
+                if (isOut(nx, ny)) continue;
+                if (visited[ny][nx]) continue;
+                if (map[ny].charAt(nx) != '.' && map[ny].charAt(nx) != 'D') continue;
+                if (waterDist[ny][nx] != -1 && waterDist[ny][nx] <= hogDist[cur.y][cur.x] + 1) continue;
+
+                visited[ny][nx] = true;
+                hogDist[ny][nx] = hogDist[cur.y][cur.x] + 1;
+                queue.add(new Coordinate(nx, ny));
+            }
+        }
+    }
+
+    private static void printAnswer() {
+        for (int y = 0; y < R; y++) {
+            for (int x = 0; x < C; x++) {
+                if (map[y].charAt(x) == 'D') {
+                    if (hogDist[y][x] == -1) System.out.println("KAKTUS");
+                    else System.out.println(hogDist[y][x]);
+                }
+            }
+        }
+    }
+
 
     private static boolean isOut(int x, int y) {
         return check(x, C) || check(y, R);
@@ -85,12 +108,11 @@ class Main {
     }
 
     private static class Coordinate {
-        int x, y, c;
+        int x, y;
 
-        public Coordinate(int x, int y, int c) {
+        public Coordinate(int x, int y) {
             this.x = x;
             this.y = y;
-            this.c = c;
         }
     }
 
@@ -101,40 +123,13 @@ class Main {
         C = r.nextInt();
         map= new String[R];
 
+        hogDist = new int[R][C];
         waterDist = new int[R][C];
-        for (int[] i :waterDist) {
-            Arrays.fill(i, Integer.MAX_VALUE);
-        }
+        visited = new boolean[R][C];
 
         for (int y = 0; y < R; y++) {
             map[y] = r.nextLine();
-            for (int x = 0; x < C; x++) {
-                if (map[y].charAt(x) == 'S') {
-                    hog = new Coordinate(x, y, 0);
-                }
-
-                if (map[y].charAt(x) == '*') {
-                    queue.add(new Coordinate(x, y, 0));
-                }
-            }
-
         }
-    }
-
-    private static boolean isWater(int x, int y) {
-        return check(x, y, WATER);
-    }
-
-    private static boolean isCave(int x, int y) {
-        return check(x, y, CAVE);
-    }
-
-    private static boolean isRock(int x, int y) {
-        return check(x, y, ROCK);
-    }
-
-    private static boolean check(int x, int y, char i) {
-        return map[y].charAt(x) == i;
     }
 
     @Test
