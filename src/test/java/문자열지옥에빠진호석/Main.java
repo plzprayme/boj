69e850f1b4fe667cf54fd2d2ee5af79b58007b38
prayme <dbfpzk142@gmail.com>
2022-01-15 00:52:52 +0900

    문자열 지옥에 빠진 호석 풀긴 풀었다. 근데 문제 드러운듯

--- src/test/java/문자열지옥에빠진호석/Main.java
+++ src/test/java/문자열지옥에빠진호석/Main.java
@@ -13,11 +13,9 @@ class Main {
     static int[] dx = {0, 1, 1, 1, 0, -1, -1, -1};
     static int[] dy = {-1, -1, 0, 1, 1, 1, 0, -1};
 
-    static Set<String> like = new HashSet<>();
+    static Map<String, Integer> counter = new HashMap<>();
 
     static InputReader r;
-
-    static int count;
 
     @Test
     public static void main(String[] args) throws IOException {
@@ -34,62 +32,43 @@ class Main {
         // 최악의 경우: N = 10, M = 10, String = aaa_aaa_aaa_a
         // 100 * 8 * 8 * 8 * 8 * 8
 
-        StringBuilder sb = new StringBuilder();
-        for (int k = 0; k < K; k++) {
-            count = 0;
-            String s = r.nextLine();
-            for (int i = 0; i < N; i++) {
-                for (int j = 0; j < M; j++) {
-                    dfs(0, i, j, s);
-                }
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                dfs(1, i, j, Character.toString(map[i].charAt(j)));
             }
-            sb.append(count).append('\n');
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < K; i++) {
+            sb.append(counter.getOrDefault(r.nextLine(), 0)).append('\n');
         }
         System.out.println(sb);
     }
 
     private static void dfs(int cur, int y, int x, String s) {
-        if (cur == s.length()) {
-            count++;
-            return;
-        }
-        if (map[y].charAt(x) != s.charAt(cur)) return;
+        counter.put(s, counter.getOrDefault(s, 0) + 1);
+
+        if (cur == 5) return;
 
         for (int i = 0; i < 8; i++) {
-            int nx = x + dx[i];
-            int ny = y + dy[i];
-
-            if (nx == -1 && ny == -1) { // 왼쪽 위
-                nx = M - 1;
-                ny = N - 1;
-
-            } else if(nx == M && ny == -1) { // 오른 위
-                nx = 0;
-                ny = N - 1;
-            } else if (nx == -1 && ny == N) { // 왼 아래
-                nx = M - 1;
-                ny = 0;
-            } else if (nx == M && ny == N) { // 오른쪽 아래
-                nx = 0;
-                ny = 0;
-            } else if (nx == -1) { //
-                nx = M - 1;
-            } else if (nx == M) {
-                nx = 0;
-            } else if (ny == -1) {
-                ny = N - 1;
-            } else if (ny == N) {
-                ny = 0;
-            }
-
-
-            dfs(cur + 1, ny, nx, s);
+            int nx = getNext(x + dx[i], M);
+            int ny = getNext(y + dy[i], N);
+            dfs(cur + 1, ny, nx, s + map[ny].charAt(nx));
         }
+    }
+
+    private static int getNext(int xy, int limit) {
+        if (xy == limit) return 0;
+        else if (xy == -1) return limit - 1;
+        else return xy;
     }
 
     private static void input() throws IOException {
         r = new InputReader("C:\\Users\\workspace\\boj\\src\\test\\java\\문자열지옥에빠진호석\\input.txt");
-        N = r.nextInt(); M = r.nextInt(); K = r.nextInt();
+        N = r.nextInt();
+        M = r.nextInt();
+        K = r.nextInt();
         map = new String[N];
         for (int i = 0; i < N; i++) map[i] = r.nextLine();
     }
