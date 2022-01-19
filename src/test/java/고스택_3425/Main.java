ed091cf9299f976c652dd586959d46cae2f97a3d
prayme <dbfpzk142@gmail.com>
2022-01-19 23:55:24 +0900

    고스택 곱셈 덧셈 오버플로우처리, 나눗셈 부호 결정

--- src/test/java/고스택_3425/Main.java
+++ src/test/java/고스택_3425/Main.java
@@ -10,13 +10,157 @@ class Main {
     static InputReader r;
     static Stack<Integer> stack = new Stack<>();
 
+    static int N;
+    static String[] operations = new String[100_000];
+
     @Test
     public static void main(String[] args) throws IOException {
-        input();
-        solution();
+        r = new InputReader("C:\\Users\\workspace\\boj\\src\\test\\java\\고스택_3425\\input.txt");
+
+        while (readOperation()) {
+            N = r.nextInt();
+            for (int i = 0; i < N; i++) {
+                process(r.nextInt());
+            }
+            System.out.println();
+        }
     }
 
-    private static void solution() {
+    private static void process(int start) {
+        Stack<Integer> stack = new Stack<>();
+        stack.add(start);
+
+        f:
+        for (int i = 0; i < 100_001; i++) {
+            String op = operations[i];
+
+            if (op.equals("END")) break;
+
+            StringTokenizer st = new StringTokenizer(op);
+            switch (st.nextToken()) {
+                case "NUM": {
+                    stack.add(Integer.parseInt(st.nextToken()));
+                    break;
+                }
+                case "POP": {
+                    if (stack.isEmpty()) {
+                        System.out.println("ERROR");
+                        break f;
+                    }
+
+                    stack.pop();
+                    break;
+                }
+                case "INV": {
+                    if (stack.isEmpty()) {
+                        System.out.println("ERROR");
+                        break f;
+                    }
+
+                    stack.add(-stack.pop());
+                    break;
+                }
+
+                case "DUP": {
+                    if (stack.isEmpty()) {
+                        System.out.println("ERROR");
+                        break f;
+                    }
+
+                    stack.add(stack.peek());
+                    break;
+                }
+
+                case "SWP": {
+                    if (stack.size() < 2) {
+                        System.out.println("ERROR");
+                        break f;
+                    }
+
+                    int first = stack.pop();
+                    int second = stack.pop();
+                    stack.add(first);
+                    stack.add(second);
+                    break;
+                }
+
+                case "ADD": {
+                    if (stack.size() < 2) {
+                        System.out.println("ERROR");
+                        break f;
+                    }
+
+                    stack.add(stack.pop() + stack.pop());
+                    break;
+                }
+
+                case "SUB": {
+                    if (stack.size() < 2) {
+                        System.out.println("ERROR");
+                        break f;
+                    }
+
+                    int first = stack.pop();
+                    int second = stack.pop();
+                    stack.add(second - first);
+                    break;
+                }
+
+                case "MUL": {
+                    if (stack.size() < 2) {
+                        System.out.println("ERROR");
+                        break f;
+                    }
+
+                    stack.add(stack.pop() * stack.pop());
+                    break;
+                }
+
+                case "DIV": {
+                    if (stack.size() < 2) {
+                        System.out.println("ERROR");
+                        break f;
+                    }
+
+                    int first = stack.pop();
+                    int second = stack.pop();
+                    stack.add(second / first);
+                    break;
+                }
+
+                case "MOD": {
+                    if (stack.size() < 2) {
+                        System.out.println("ERROR");
+                        break f;
+                    }
+
+                    int first = stack.pop();
+                    int second = stack.pop();
+                    stack.add(second % first);
+                    break;
+                }
+
+
+            }
+        }
+
+
+        if (stack.size() == 1) System.out.println(stack.pop());
+        else System.out.println("ERROR");
+    }
+
+    private static boolean readOperation() throws IOException {
+        int count = 0;
+        while (true) {
+            String op = r.nextLine();
+            if (op.equals("")) continue;
+            operations[count++] = op;
+            if (op.equals("END")) return true;
+            else if (op.equals("QUIT")) return false;
+        }
+    }
+
+    private static void solution() throws IOException {
         // 고스택은 숫자만 저장할 수 있다.
 
         // NUM X = 스택의 가장 위에 저장
@@ -30,11 +174,112 @@ class Main {
         // DIV: 첫번째 두번쨰 나눈 몫 저장한다. 두번쨰가 피제수 첫째수가 제수
         // MOD: 첫번째 숫자로 두번째 숫자를 나눈 나머지 저장 두번째 숫자가 피제수, 첫번째 숫자가 제수
 
+        Stack<Integer> stack = new Stack<>();
+        stack.add(r.nextInt());
+
+        w:
+        while (true) {
+            StringTokenizer st = new StringTokenizer(r.nextLine());
+            String op = st.nextToken();
+
+            switch (op) {
+
+                case "DUP": {
+                    stack.add(stack.peek());
+                    break;
+                }
+
+                case "NUM": {
+                    stack.add(Integer.parseInt(st.nextToken()));
+                    break;
+                }
+
+                case "POP": {
+                    if (stack.isEmpty()) {
+                        System.out.println("ERROR");
+                        break;
+                    }
+                    stack.pop();
+                    break;
+                }
+
+                case "INT": {
+                    if (stack.isEmpty()) {
+                        System.out.println("ERROR");
+                        break;
+                    }
+                    stack.add(-stack.pop());
+                    break;
+                }
+
+                case "ADD":
+                case "SWP":
+                case "SUB":
+                case "MUL":
+                case "DIV":
+                case "MOD": {
+                    if (stack.size() < 2) {
+                        System.out.println("ERROR");
+                        break w;
+                    }
+
+                    int first = stack.pop();
+                    int second = stack.pop();
+
+                    if (op.equals("SWP")) {
+                        stack.add(first);
+                        stack.add(second);
+                        break;
+                    }
+
+
+                    int result = 0;
+                    switch (op) {
+                        case "ADD": {
+                            result = first + second;
+                            break;
+                        }
+                        case "SUB": {
+                            result = second - first;
+                            break;
+                        }
+                        case "MUL": {
+                            result = first * second;
+                            break;
+                        }
+                        case "DIV": {
+                            result = second / first;
+                            break;
+                        }
+                        case "MOD": {
+                            result = second % first;
+                            break;
+                        }
+                    }
+
+                    stack.add(result);
+                }
+
+                case "END": {
+                    break w;
+                }
+
+            }
+
+        }
+
+        if (stack.size() != 1) System.out.println("ERROR");
+        else System.out.println(stack.pop());
+
 
     }
-    private static void input() throws IOException {
-        r = new InputReader("C:\\Users\\workspace\\boj\\src\\test\\java\\고스택_3425\\input.txt");
 
+    private static void input(String op) throws IOException {
+        operations[0] = op;
+        for (int i = 1; ; i++) {
+            if (operations[i].equals("END")) return;
+            operations[i] = r.nextLine();
+        }
     }
 
     private static class InputReader {
@@ -61,6 +306,10 @@ class Main {
 
         public char[] nextCharArr() throws IOException {
             return r.readLine().toCharArray();
+        }
+
+        public String nextLine() throws IOException {
+            return r.readLine();
         }
     }
 
