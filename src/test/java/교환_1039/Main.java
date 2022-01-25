f37b1dc2e836f8961a99ccd3ee8c118b7d98460a
prayme <dbfpzk142@gmail.com>
2022-01-25 23:26:48 +0900

    교환 1039 Set으로 dp 왜 틀렸을까?

--- src/test/java/교환_1039/Main.java
+++ src/test/java/교환_1039/Main.java
@@ -8,9 +8,13 @@ import org.junit.jupiter.api.Test;
 class Main {
 
     static int N, M;
-    static char[] K;
+    static String K;
+//    static char[] K;
 
     static int answer = -1;
+
+    static Set<String> oddDp = new HashSet<>();
+    static Set<String> evenDp = new HashSet<>();
 
     @Test
     public static void main(String[] args) throws IOException {
@@ -25,39 +29,51 @@ class Main {
         // 7 자리의 모든 숫자를 바꿔본다면? 완전탐색의 시간 복잡도는?
         // O(M-1! * K) = O(6! * K) = O(6 * 5 * 4 * 3 * 2 * 10) = 7200 가능.
 
+        // 캐싱을 통해 메모리를 적게 써보자
+
         backtracking(0, K);
         System.out.println(answer);
     }
 
-    private static void backtracking(int m, char[] k) {
+    private static void backtracking(int m, String k) {
         if (M == m) {
-            answer = Math.max(Integer.parseInt(String.valueOf(k)), answer);
+            answer = Math.max(Integer.parseInt(k), answer);
         } else {
-            for (int i = 0; i < K.length; i++) {
-                for (int j = i + 1; j < K.length; j++) {
-                    swap(k, i, j);
-                    if (k[0] == '0') {
-                        swap(k, j, i);
-                        continue;
+            boolean isOdd = m % 2 == 1;
+            for (int i = 0; i < K.length(); i++) {
+                for (int j = i + 1; j < K.length(); j++) {
+                    String next = swap(k, i, j);
+                    if (next.charAt(0) == '0') continue;
+                    if (isOdd) {
+                        if (oddDp.contains(next)) {
+                            continue;
+                        }
+                        oddDp.add(next);
+                    } else {
+                        if (evenDp.contains(next)) {
+                            continue;
+                        }
+                        evenDp.add(next);
                     }
-                    backtracking(m + 1, k);
-                    swap(k, j, i);
+                    backtracking(m + 1, next);
                 }
             }
         }
     }
 
-    private static char[] swap(char[] s, int left, int right) {
-        char tmp = s[left];
-        s[left] = s[right];
-        s[right] = tmp;
-        return s;
+    private static String swap(String s, int left, int right) {
+        char[] arr = s.toCharArray();
+        char tmp = arr[left];
+        arr[left] = arr[right];
+        arr[right] = tmp;
+        return String.valueOf(arr);
     }
 
     private static void input() throws IOException {
         InputReader r = new InputReader("C:\\Users\\workspace\\boj\\src\\test\\java\\교환_1039\\input.txt");
         N = r.nextInt(); M = r.nextInt();
-        K = String.valueOf(N).toCharArray();
+        K = String.valueOf(N);
+//        K = String.valueOf(N).toCharArray();
     }
 
     private static class InputReader {
