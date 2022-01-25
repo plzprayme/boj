c0350c2b29e197c46d2e9166f32e28308dcff419
prayme <dbfpzk142@gmail.com>
2022-01-25 23:16:41 +0900

    교환 1039 완탐 메모리 초과

--- src/test/java/교환_1039/Main.java
+++ src/test/java/교환_1039/Main.java
@@ -8,6 +8,9 @@ import org.junit.jupiter.api.Test;
 class Main {
 
     static int N, M;
+    static char[] K;
+
+    static int answer = -1;
 
     @Test
     public static void main(String[] args) throws IOException {
@@ -22,12 +25,39 @@ class Main {
         // 7 자리의 모든 숫자를 바꿔본다면? 완전탐색의 시간 복잡도는?
         // O(M-1! * K) = O(6! * K) = O(6 * 5 * 4 * 3 * 2 * 10) = 7200 가능.
 
+        backtracking(0, K);
+        System.out.println(answer);
+    }
 
+    private static void backtracking(int m, char[] k) {
+        if (M == m) {
+            answer = Math.max(Integer.parseInt(String.valueOf(k)), answer);
+        } else {
+            for (int i = 0; i < K.length; i++) {
+                for (int j = i + 1; j < K.length; j++) {
+                    swap(k, i, j);
+                    if (k[0] == '0') {
+                        swap(k, j, i);
+                        continue;
+                    }
+                    backtracking(m + 1, k);
+                    swap(k, j, i);
+                }
+            }
+        }
+    }
+
+    private static char[] swap(char[] s, int left, int right) {
+        char tmp = s[left];
+        s[left] = s[right];
+        s[right] = tmp;
+        return s;
     }
 
     private static void input() throws IOException {
         InputReader r = new InputReader("C:\\Users\\workspace\\boj\\src\\test\\java\\교환_1039\\input.txt");
         N = r.nextInt(); M = r.nextInt();
+        K = String.valueOf(N).toCharArray();
     }
 
     private static class InputReader {
